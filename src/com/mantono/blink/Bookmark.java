d2d61e316e477d0354c76d6dbf4a71a52d674cbc
Anton Österberg <anton.osterberg@gmail.com>
2016-05-27 03:21:42 +0200

    Updated computation of hash, now it works properly.

--- src/com/mantono/blink/Bookmark.java
+++ src/com/mantono/blink/Bookmark.java
@@ -88,7 +88,17 @@ public class Bookmark implements Comparable<Bookmark>, Serializable
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
 		final String urlText = url.toString();
 		byte[] hash = digest.digest(urlText.getBytes(StandardCharsets.UTF_8));
-		return new String(hash);
+		StringBuffer hexString = new StringBuffer();
+
+		for(int i = 0; i < hash.length; i++)
+		{
+			String hex = Integer.toHexString(0xff & hash[i]);
+			if(hex.length() == 1)
+				hexString.append('0');
+			hexString.append(hex);
+		}
+
+		return hexString.toString();
 	}
 
 	@Override
