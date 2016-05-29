9dcf33f25482f6eb4fc2e4c55c05eabb747cb7ec
Anton Österberg <anton.osterberg@gmail.com>
2016-05-29 22:54:38 +0200

    Index now supports loading and saving previously built index of bookmarks.

--- src/com/mantono/blink/Index.java
+++ src/com/mantono/blink/Index.java
@@ -1,7 +1,11 @@
 package com.mantono.blink;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -9,7 +13,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,17 +21,30 @@ public class Index implements Serializable
 {
 	private static final Path ROOT = Paths.get(System.getProperty("user.home") + "/.blink");
 	private static final File DIR = new File(ROOT + "/bookmarks");
+	private static final File SAVED_INDEX = new File(ROOT + "/.index");
+	private final Set<Bookmark> bookmarks;
+	private int loadedHashCode = 0;
 	private Instant indexLastBuilt;
+	
+	public Index() throws IOException
+	{
+		this.bookmarks = new HashSet<Bookmark>();
+		this.indexLastBuilt = Instant.ofEpochMilli(0);
+	}
+	
+	public int size()
+	{
+		return bookmarks.size();
+	}
 	
 	public Set<Bookmark> findBookmarks()
 	{
-		Set<Bookmark> assets = new HashSet<Bookmark>();
 		if(DIR.exists())
 		{
-			if(indexLastBuilt == null)
-				loadFiles(DIR, assets);
-			else if(DIR.lastModified() > indexLastBuilt.toEpochMilli())
-				loadFiles(DIR, assets);
+			if(DIR.lastModified() > indexLastBuilt.toEpochMilli())
+				loadFiles(DIR, bookmarks);
+			else
+				return bookmarks;
 		}
 		else
 		{
@@ -36,12 +52,21 @@ public class Index implements Serializable
 			System.exit(1);
 		}
 		indexLastBuilt = Instant.now();
-		return assets;
+		
+		return bookmarks;
 	}
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException, ClassNotFoundException
 	{
-		Set<Bookmark> bms = new Index().findBookmarks();
+		Index index;
+		
+		if(SAVED_INDEX.exists())
+			index = loadIndex();
+		else
+			index = new Index();
+		
+		Set<Bookmark> bms = index.findBookmarks();
+		
 		for(Bookmark bookmark : bms)
 		{
 			Set<String> labels = bookmark.getLabels();
@@ -54,9 +79,38 @@ public class Index implements Serializable
 				}
 			}
 		}
+		
+		if(index.isUpdatedSinceLoad())
+			saveIndex(index);
 	}
 
-	private void loadFiles(File dir, Set<Bookmark> assets)
+	private boolean isUpdatedSinceLoad()
+	{
+		return bookmarks.hashCode() != loadedHashCode;
+	}
+
+	private static void saveIndex(Index index) throws IOException
+	{
+		FileOutputStream fos = new FileOutputStream(SAVED_INDEX);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(index);
+	}
+
+	private static Index loadIndex() throws IOException, ClassNotFoundException
+	{
+		FileInputStream fis = new FileInputStream(SAVED_INDEX);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Index index = (Index) ois.readObject();
+		index.updateHashCode();
+		return index;
+	}
+
+	private void updateHashCode()
+	{
+		this.loadedHashCode = bookmarks.hashCode();
+	}
+
+	private Set<Bookmark> loadFiles(File dir, Set<Bookmark> bookmarks)
 	{
 		File[] directoryListing = dir.listFiles();
 		if(directoryListing != null)
@@ -64,14 +118,17 @@ public class Index implements Serializable
 			for(File file : directoryListing)
 			{
 				if(file.isDirectory())
-					loadFiles(file, assets);
+					loadFiles(file, bookmarks);
 				else
 				{
 					Bookmark bookmark = parseBookmarkFile(file);
-					assets.add(bookmark);
+					if(bookmark != null)
+						bookmarks.add(bookmark);
 				}
 			}
 		}
+		
+		return bookmarks;
 	}
 
 	private Bookmark parseBookmarkFile(File file)
@@ -89,6 +146,10 @@ public class Index implements Serializable
 		{
 			System.err.println("Error reading " + file.getAbsolutePath());
 			e.printStackTrace();
+		}
+		catch(IndexOutOfBoundsException e)
+		{
+			System.err.println("Illegal file " + file + " ignored.");
 		}
 		return null;
 	}
